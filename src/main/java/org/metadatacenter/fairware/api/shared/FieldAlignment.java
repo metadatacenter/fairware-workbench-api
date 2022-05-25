fcfe21515441d4c40a17a0dee2e38b5b7f895533
Josef Hardi <josef.hardi@gmail.com>
2022-05-25 10:51:31 -0700

    fix: Fix object serialization and deserialization

--- src/main/java/org/metadatacenter/fairware/api/shared/FieldAlignment.java
+++ src/main/java/org/metadatacenter/fairware/api/shared/FieldAlignment.java
@@ -1,5 +1,7 @@
 package org.metadatacenter.fairware.api.shared;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.auto.value.AutoValue;
 
 import javax.annotation.Nonnull;
@@ -7,17 +9,26 @@ import javax.annotation.Nonnull;
 @AutoValue
 public abstract class FieldAlignment {
 
-  public static FieldAlignment create(double similarityScore,
-                                      @Nonnull String metadataFieldPath,
-                                      @Nonnull String templateFieldPath) {
+  private static final String SIMILARITY_SCORE = "similarityScore";
+  private static final String METADATA_FIELD_PATH = "metadataFieldPath";
+  private static final String TEMPLATE_FIELD_PATH = "templateFieldPath";
+
+  @Nonnull
+  @JsonCreator
+  public static FieldAlignment create(@JsonProperty(SIMILARITY_SCORE) double similarityScore,
+                                      @Nonnull @JsonProperty(METADATA_FIELD_PATH) String metadataFieldPath,
+                                      @Nonnull @JsonProperty(TEMPLATE_FIELD_PATH) String templateFieldPath) {
     return new AutoValue_FieldAlignment(similarityScore, metadataFieldPath, templateFieldPath);
   }
 
+  @JsonProperty(SIMILARITY_SCORE)
   public abstract double getSimilarityScore();
 
   @Nonnull
+  @JsonProperty(METADATA_FIELD_PATH)
   public abstract String getMetadataFieldPath();
 
   @Nonnull
+  @JsonProperty(TEMPLATE_FIELD_PATH)
   public abstract String getTemplateFieldPath();
 }
