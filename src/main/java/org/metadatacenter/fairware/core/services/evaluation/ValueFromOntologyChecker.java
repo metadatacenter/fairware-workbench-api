5560f5a0911e8376041495b2d8e5b73014cbef98
Josef Hardi <josef.hardi@gmail.com>
2022-06-01 15:22:04 -0700

    fix: run BioPortal search when query is not empty

--- src/main/java/org/metadatacenter/fairware/core/services/evaluation/ValueFromOntologyChecker.java
+++ src/main/java/org/metadatacenter/fairware/core/services/evaluation/ValueFromOntologyChecker.java
@@ -1,13 +1,11 @@
 package org.metadatacenter.fairware.core.services.evaluation;
 
-import com.google.common.collect.Lists;
 import org.apache.http.HttpException;
 import org.metadatacenter.fairware.api.response.EvaluationReportItem;
 import org.metadatacenter.fairware.api.response.action.RepairAction;
 import org.metadatacenter.fairware.api.response.action.SuggestedOntologyTerm;
 import org.metadatacenter.fairware.api.response.issue.IssueType;
 import org.metadatacenter.fairware.api.response.issue.MetadataIssue;
-import org.metadatacenter.fairware.config.bioportal.BioportalConfig;
 import org.metadatacenter.fairware.core.services.bioportal.BioportalService;
 import org.metadatacenter.fairware.core.util.GeneralUtil;
 import org.metadatacenter.fairware.core.util.cedar.extraction.model.MetadataFieldInfo;
@@ -31,26 +29,31 @@ public class ValueFromOntologyChecker {
     Optional<Object> value = metadataField.getValue();
     if (value.isPresent()) {
       var valueString = String.valueOf(value.get());
-      var results = bioportalService.search(valueString, ontology);
-      if (results.getTotalCount() == 0) {
-        Optional<String> prefLabel = metadataField.getPrefLabel();
-        if (prefLabel.isPresent()) {
-          results = bioportalService.search(prefLabel.get(), ontology);
-          if (results.getTotalCount() == 0) {
-            return Optional.empty();
+      if (!valueString.isEmpty()) {
+        var results = bioportalService.search(valueString, ontology);
+        if (results.getTotalCount() == 0) {
+          Optional<String> prefLabel = metadataField.getPrefLabel();
+          if (prefLabel.isPresent()) {
+            var prefLabelString = prefLabel.get();
+            if (!prefLabelString.isEmpty()) {
+              results = bioportalService.search(prefLabelString, ontology);
+              if (results.getTotalCount() == 0) {
+                return Optional.empty();
+              }
+            }
           }
         }
+        var firstOntologyTerm = results.getCollection().stream().findFirst()
+            .map(bpClass -> SuggestedOntologyTerm.create(
+                bpClass.getId(),
+                bpClass.getPrefLabel(),
+                ontology)).get();
+        var report = EvaluationReportItem.create(
+            GeneralUtil.generateFullPathDotNotation(metadataField),
+            MetadataIssue.create(IssueType.VALUE_NOT_ONTOLOGY_TERM),
+            RepairAction.ofReplaceMetadataValueWithOntologyTerm(firstOntologyTerm));
+        return Optional.of(report);
       }
-      var firstOntologyTerm = results.getCollection().stream().findFirst()
-          .map(bpClass -> SuggestedOntologyTerm.create(
-              bpClass.getId(),
-              bpClass.getPrefLabel(),
-              ontology)).get();
-      var report = EvaluationReportItem.create(
-          GeneralUtil.generateFullPathDotNotation(metadataField),
-          MetadataIssue.create(IssueType.VALUE_NOT_ONTOLOGY_TERM),
-          RepairAction.ofReplaceMetadataValueWithOntologyTerm(firstOntologyTerm));
-      return Optional.of(report);
     }
     return Optional.empty();
   }
