a9252e8014999a6e203e6cf321b73c0f657d022d
Josef Hardi <josef.hardi@gmail.com>
2022-07-06 16:45:58 -0700

    refactor: change field name

--- src/main/java/org/metadatacenter/fairware/api/response/evaluation/EvaluationReportItem.java
+++ src/main/java/org/metadatacenter/fairware/api/response/evaluation/EvaluationReportItem.java
@@ -3,25 +3,25 @@ package org.metadatacenter.fairware.api.response.evaluation;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.auto.value.AutoValue;
-import org.metadatacenter.fairware.shared.RepairAction;
 import org.metadatacenter.fairware.shared.MetadataIssue;
+import org.metadatacenter.fairware.shared.RepairAction;
 
 import javax.annotation.Nonnull;
 
 @AutoValue
 public abstract class EvaluationReportItem {
 
-  private static final String ISSUE_DETAILS = "issueDetails";
+  private static final String METADATA_ISSUE = "metadataIssue";
   private static final String REPAIR_ACTION = "repairAction";
 
   @JsonCreator
-  public static EvaluationReportItem create(@Nonnull @JsonProperty(ISSUE_DETAILS) MetadataIssue metadataIssue,
+  public static EvaluationReportItem create(@Nonnull @JsonProperty(METADATA_ISSUE) MetadataIssue metadataIssue,
                                             @Nonnull @JsonProperty(REPAIR_ACTION) RepairAction repairAction) {
     return new AutoValue_EvaluationReportItem(metadataIssue, repairAction);
   }
 
   @Nonnull
-  @JsonProperty(ISSUE_DETAILS)
+  @JsonProperty(METADATA_ISSUE)
   public abstract MetadataIssue getMetadataIssue();
 
   @Nonnull
